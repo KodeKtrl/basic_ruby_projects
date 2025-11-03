3fd8f9082e296288b1b5d4b265c22721b4ee92f6
KodeKtrl <202951161+KodeKtrl@users.noreply.github.com>
2025-11-03 15:29:31 +0530

    improved code

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -1,5 +1,25 @@
 def caesar_ciper(word, value = 0)
    result = []
    word = word.split("")
+   word.map! {|element| element.ord}
    
+   value = value % 26
+
+   word.each do |element|
+    if element.between?(65, 90)
+      diff = element + value
+      diff -=26 if diff > 90
+      result.push(diff)
+    elsif element.between?(97,122)
+        diff = element + value
+        diff -=26 if diff > 122
+        result.push(diff)
+    else
+        result.push(element)
+    end
+   end
+   result.map! {|c| c.chr}
+   result.join
 end
+
+p caesar_ciper("Hello World z!", 5)
