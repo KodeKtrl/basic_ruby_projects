e0a389baa82f35b67c1d25fae4c7edf4f41b2ccd
KodeKtrl <202951161+KodeKtrl@users.noreply.github.com>
2025-11-03 15:03:23 +0530

    add smallcase and symbol cipher

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -3,20 +3,34 @@ def caesar_ciper(word, value = 0)
    word = word.split("")
    word.map! {|element| element.ord}
    word.map! {|element| element}
-   for element in word do
-       if element + value >= 90
-        difference = 90 - element
-        element = 64 - difference + value
-        result.push(element)
-       elsif element <= 64
-        element = 64 + value
-        result.push(element)
-       else 
-        result.push(element + value)
-       end
-   end
-   result.map! {|element| element.chr}
-   result
+    for element in word do
+        if element >= 65 and element <= 90
+            if element + value >= 90
+                difference = 90 - element
+                element = 65 - difference + (value - 1)
+                result.push(element)
+            elsif element + value >= 65
+                element = element + value
+                result.push(element)
+            end
+        end
+        if element >= 97 and element <= 122
+            if element + value >= 122
+              difference = 122 - element
+              element = 97 - difference + (value - 1)
+              result.push(element)
+            elsif element + value >=97
+                element = element + value
+                result.push(element)
+            end
+        end
+        if element.between?(65, 97) == false and element.between?(97, 122) == false 
+          result.push(element)
+        end
+
+    end
+        result.map! {|element| element.chr}
+        result.join
 end
 
-p caesar_ciper("Y", 4)
+p caesar_ciper("What is up?", 4)
