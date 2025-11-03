b07c573f6a77a3e459dc361f5970db0fb1cc533b
KodeKtrl <202951161+KodeKtrl@users.noreply.github.com>
2025-11-03 12:05:13 +0530

    fix wrap issue

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -4,19 +4,19 @@ def caesar_ciper(word, value = 0)
    word.map! {|element| element.ord}
    word.map! {|element| element}
    for element in word do
-       if element + value >= 90 
-          element = 64 + value
-          result.push(element)
-       elsif element <= 65
-          element = 64 + value
-          result.push(element)
+       if element + value >= 90
+        difference = 90 - element
+        element = 64 - difference + value
+        result.push(element)
+       elsif element <= 64
+        element = 64 + value
+        result.push(element)
        else 
-          result.push(element + value)
-
+        result.push(element + value)
        end
    end
    result.map! {|element| element.chr}
    result
 end
 
-p caesar_ciper("ZY", 4)
+p caesar_ciper("Y", 4)
