7976a9d1f18d11eb1fcb60936b0e9cd9c0c91843
KodeKtrl <202951161+KodeKtrl@users.noreply.github.com>
2025-11-04 12:06:22 +0530

    refactored

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -1,21 +1,26 @@
-def stock_picker(array)
+def stock_picker(prices)
   #debugger
-  highest_value = 0
-  lowest_value = 0
-  difference = highest_value - lowest_value
-  array.each_with_index do |current_value, index|
-    if index == 0
-      lowest_value = current_value
-    elsif current_value < lowest_value and (difference > highest_value - current_value)
-      lowest_value = current_value
-    elsif current_value > highest_value
-      highest_value = current_value
-    end
-  end 
-  result = [array.index(lowest_value), array.index(highest_value)]
-  return result
+ sell_day = 0
+ buy_day = 0
+ highest_profit = 0
+
+ lowest_day = 0
+ lowest_price = prices[0]
+
+ prices.each_with_index do |price, index|
+   if price < lowest_price
+     lowest_price = price
+     lowest_day = index
+   end
+
+   profit = price - lowest_price
+   if profit > highest_profit
+     highest_profit = profit
+     sell_day = index
+     buy_day = lowest_day
+   end
+ end
+ [buy_day, sell_day]
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
-
-#[17,3,6,9,15,8,6,1,10]
