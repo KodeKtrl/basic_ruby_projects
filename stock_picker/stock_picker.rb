ee8fbff2cd21e38af3d6c74ce4ba4c6408787ff2
KodeKtrl <202951161+KodeKtrl@users.noreply.github.com>
2025-11-04 11:53:01 +0530

    Fix buy day

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -1,18 +1,17 @@
 def stock_picker(array)
   #debugger
   highest_value = 0
-  current_high = 0
   lowest_value = 0
+  difference = highest_value - lowest_value
   array.each_with_index do |current_value, index|
     if index == 0
       lowest_value = current_value
-    elsif (current_value < lowest_value)
+    elsif current_value < lowest_value and (difference > highest_value - current_value)
       lowest_value = current_value
     elsif current_value > highest_value
       highest_value = current_value
-      current_high = current_value
     end
-  end and (current_high == highest_value)
+  end 
   result = [array.index(lowest_value), array.index(highest_value)]
   return result
 end
