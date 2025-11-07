35f796dbe5493e85db072d2cda781a41295d29ad
KodeKtrl <202951161+KodeKtrl@users.noreply.github.com>
2025-11-07 17:36:03 +0530

    clean code

--- tic-tac-toe/lib/board.rb
+++ tic-tac-toe/lib/board.rb
@@ -7,23 +7,19 @@ class MyBoard # rubocop:disable Style/Documentation
     @arr = arr
   end
 
-  def create_board(arr) # rubocop:disable Metrics/MethodLength
-    arr.each do |num|
-      if ((num + 1) % 3).zero?
-        print " #{num}"
-      else
-        print " #{num} |"
-      end
-      if num + 1 == arr.length
+  def create_board(arr = @arr)
+    arr.each_with_index do |num, index|
+      print " #{num}"
+      print ' |' unless ((index + 1) % 3).zero?
+
+      if ((index + 1) % 3).zero?
         puts ''
-      else
-        puts ''
-        puts '___|___|___'
+        puts '___|___|___' unless index == arr.length - 1
       end
     end
   end
 
-  def game_over(combinations, player, computer, board)
+  def game_over(combinations, player, computer, board) # rubocop:disable Metrics/CyclomaticComplexity
     combinations.each do |combination|
       return 'Player wins!' if combination.all? { |num| player.include?(num) }
       return 'Computer wins!' if combination.all? { |num| computer.include?(num) }
@@ -31,11 +27,11 @@ class MyBoard # rubocop:disable Style/Documentation
 
     return "It's a draw!" if board.none? { |n| n.is_a?(Integer) }
 
-    update_board(board)
+    update_board
     'The game continues!'
   end
 
-  def update_board(board)
-    create_board(board)
+  def update_board
+    create_board(@arr)
   end
 end
