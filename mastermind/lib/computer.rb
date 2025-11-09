e16970bcb70e19275ec14867db4e6c97e7b18328
KodeKtrl <202951161+KodeKtrl@users.noreply.github.com>
2025-11-09 16:08:50 +0530

    add computer logic

--- mastermind/lib/computer.rb
+++ mastermind/lib/computer.rb
@@ -2,17 +2,46 @@
 
 class Computer # rubocop:disable Style/Documentation
   def make_code(options)
-    code = []
-    4.times do |i|
-      i = options.sample # rubocop:disable Lint/ShadowedArgument
-      code.push(i)
-    end
+    options.sample(4)
   end
 
-  def guess_code(code, options)
-    possible_codes = []
-    validator = { correct: 0, close: 0 }
-    all_codes = options.repeated_permutations(4).to_a # makes all possible combinations with all options
-    computer_code = [0, 0, 1, 1]
+  def guess_code(secret_code)
+    numbers = [1, 2, 3, 4, 5, 6]
+    all_codes = numbers.repeated_permutation(4).to_a
+    guess = [1, 1, 2, 2]
+    possible_codes = all_codes.dup
+
+    12.times do |round|
+      result = validator(secret_code, guess)
+
+      return puts "Victory #{guess} is the right answer. You took #{round + 1} guesses" if result[:correct] == 4
+
+      possible_codes.select! { |code| validator(code, guess) == result }
+
+      puts " #{guess} incorrect! #{12 - round} guesses left."
+      puts "Black pegs: #{result[:correct]}, White pegs: #{result[:close]}"
+      guess = possible_codes[0]
+    end
+    puts 'You failed to guess...'
+  end
+
+  def validator(code, guess) # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
+    checker = { correct: 0, close: 0 }
+    code_copy = code.dup
+    guess_copy = guess.dup
+
+    code_copy.each_with_index do |_num, index|
+      if code_copy[index] == guess_copy[index]
+        checker[:correct] += 1
+        code_copy[index] = guess_copy[index] = nil
+      end
+    end
+    guess_copy.compact.each do |num|
+      if (ind = code_copy.index(num))
+        checker[:close] += 1
+        code_copy[ind] = nil
+      end
+    end
+    checker
   end
 end
--- mastermind/lib/game.rb
+++ mastermind/lib/game.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
+require_relative 'computer'
+require_relative 'player'
+
 class Game # rubocop:disable Style/Documentation
-  options = ['🔴', '🔵', '🟢', '🟣', '⚫', '⚪'] # rubocop:disable Lint/UselessAssignment
-  value = options.sample
-  puts value
+  def initialize
+    @player = Player.new
+    @computer = Computer.new
+  end
+
+  def play_game
+    puts 'Do you want to be the codemaker or the codebreaker?'
+    puts 'Press 1 for codemaker and 2 for codebreaker'
+    answer = gets.chomp.to_i
+    if answer == 1
+      code = @player.make_code
+      @computer.guess_code(code)
+    else
+      code = @computer.make_code([1, 2, 3, 4, 5, 6])
+      @player.guess_code(code)
+    end
+  end
 end
